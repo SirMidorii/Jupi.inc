bb71fceb02fe1873fe6d8b3d2a3e0e77e7103c66
JurIVoelker <Juri.Voelker@gmx.de>
2022-12-25 00:55:28 +0100

    Brain age rework
    Features:
    - New Design
    - Added Font Animation
    - Added Body Animation

--- Trashsites.greate-site.net/T-How-Old-is-your-Brain/index.js
+++ Trashsites.greate-site.net/T-How-Old-is-your-Brain/index.js
@@ -1,20 +1,25 @@
 var count = 0;
-var age = 40;
+var age = 61;
 
 function pressbutton() {
   count++;
   age -= 6;
   var random = Math.floor(Math.random()*1750);
-  var random2 = Math.floor(Math.random()*850);
-  document.getElementById("honix").style.marginLeft = random + "px";
-  document.getElementById("honix").style.marginTop = random2 + "px";
-  document.getElementById("age").innerHTML = "Your Brain Age: " + age + " Years";
+  var random2 = Math.floor(Math.random()*600);
+  document.getElementById("hoverDiv").style.marginLeft = random + "px";
+  document.getElementById("hoverDiv").style.marginTop = random2 + "px";
+  document.getElementById("age").innerHTML = age + " Years";
 }
 
 function onHover() {
   if (count >= 5) {
+    
     pressbutton();
+    if (count > 6) {
+      document.body.style.animationName = "bgAnim";
+      document.body.style.animationDuration = 5/count + "s";
+      document.getElementById("age").style.animationDuration = 5/count+"s";
+    }
     age *= 3;
-    document.getElementById("age").style.fontSize = age+"px";
   }
 }
