98f3e93b26bfb5ba52f37f2207d5182ab555442f
JurIVoelker <Juri.Voelker@gmx.de>
2022-12-25 17:49:15 +0100

    Snakes
    Bugfix again

--- Trashsites.greate-site.net/S-Watch-out-for-the-snakes/T-Watch-Out-For-The-Snakes/index.js
+++ Trashsites.greate-site.net/S-Watch-out-for-the-snakes/T-Watch-Out-For-The-Snakes/index.js
@@ -5,19 +5,43 @@ function gameStart() {
     document.getElementById("wrapper").style.display = "none";
     document.getElementById("bg").style.display = "none";
     document.getElementById("game").style.display = "inline";
-    //update();
+    document.getElementById("row1-1").style.backgroundColor = "blue";
+    document.getElementById("row1-1").style.backgroundImage = url();
+
+
+    update();
   },500);
 
 }
 
-var yPos = 0;
-
 function update() {
   setTimeout(function(){
-    yPos += 50;
+
     update();
   },500);
 
   //document.getElementById("item").style.transform = "translateX(" + yPos + "px)";
-
 }
+
+//document.addEventListener('keydown', function(event) {
+//  if (event.defaultPrevented) {
+//  return; // Do nothing if the event was already processed
+//}
+
+// (event.key) {
+//  case "ArrowDown":
+//    document.getElementById().style.transform = rotateX(90);
+//    break;
+//  case "ArrowUp":
+//    // code for "up arrow" key press.
+//    break;
+//  case "ArrowLeft":
+//    // code for "left arrow" key press.
+//    break;
+//  case "ArrowRight":
+//    // code for "right arrow" key press.
+//    break;
+//  default:
+//    return; // Quit when this doesn't handle the key event.
+//  }
+//}
